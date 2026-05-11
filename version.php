6682b962f59721d7c563dfc673396bbc280d688a
siddiquitaha867-ai <siddiquitaha867@gmail.com>
2026-05-12 01:26:49 +0500

    Style site administration landing

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026043032;
+$plugin->version   = 2026043033;
 $plugin->requires  = 2024100700; // Moodle 4.5.
 $plugin->component = 'theme_baitalgahwa';
 $plugin->maturity  = MATURITY_STABLE;
